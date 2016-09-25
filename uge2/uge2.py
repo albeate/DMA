65ff1aaa46a3ced285584a488920f71ca79cf28d
albeate <beateberendt12@gmail.com>
2016-09-25 10:49:04 +0200

    ulalaKAAK

--- uge2/uge2.py
+++ uge2/uge2.py
@@ -6,7 +6,10 @@ A = [2,8,4,3,6]
 def inversion(A):
 	n = len(A)
 	m = 0
-	return sum([m+1 for i in range(n-1) for j in range(n-1) if A[i] > A[j] and i < j])
-print "antallet af insertions:",inversion(A),"in",time.clock()-start_time,"seconds"
+	for i in range(n-1):
+		for j in range(i+1,n-1):
+			if A[i] > A[j]:
+				m += 1
+	return m
 
-> antallet af insertions: 3 in 3.7e-05 seconds
+print "antallet af insertions:",inversion(A),"in",time.clock()-start_time,"seconds"
